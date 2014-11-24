a17d60eb81f1a187f33a43c4dc974d1f6f4775f5
dc7878 <1026961500@qq.com>
2014-11-24 14:24:18 +0800

    修改date的格式

--- src/ayr/cc/dc/chatrobot/utils/DateUtils.java
+++ src/ayr/cc/dc/chatrobot/utils/DateUtils.java
@@ -5,8 +5,7 @@ import java.util.Date;
 
 public class DateUtils {
    public static String getDateString(Date date){
-//	   SimpleDateFormat format = new SimpleDateFormat("yyy年mm月dd日  hh:mm:ss");
-	   SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	   SimpleDateFormat format = new SimpleDateFormat("yyy年MM月dd日 HH:mm:ss");
 	   return format.format(date);
    }
 }
